6cd0042b82fddb78939da44589047cd7c589a5e0
Hamed Gorjiara <hgorjiar@uci.edu>
2021-04-22 17:15:23 -0700

    Editing run.sh script

--- run.sh
+++ run.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-export LD_LIBRARY_PATH=/scratch/nvm/pmcheck/bin/:/scratch/nvm/pmdk/src/nondebug
+export NDCTL_ENABLE=n
+export LD_LIBRARY_PATH=/scratch/nvm/pmcheck/bin/:/scratch/nvm/pmdk/src/debug
 # For Mac OSX
-export DYLD_LIBRARY_PATH=/scratch/nvm/pmcheck/bin/:/scratch/nvm/pmdk/src/nondebug
-#export PMCheck="-d$3 -p1 -v3 -e -c3"
-#export PMCheck="-d$3 -p1 -v3 -e"
-export PMCheck="-p2 -v3"
-echo "src/redis-server ./redis.conf workload/pmfile.file workload/tmp.log 8mb"
-#./src/redis-server ./redis.conf workload/pmfile.file workload/tmp.log 8mb
+export DYLD_LIBRARY_PATH=/scratch/nvm/pmcheck/bin/
+#export PMCheck="-d/ramfs/redis.pm -x5 -p1 -y -r15000000"
+export PMCheck="-d/ramfs/redis.pm -x5 -p1 -y -r4000000"
+echo "./run.sh src/redis-server ./redis.conf"
 echo $@
 
 $@
